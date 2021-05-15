176923dfdb7615a55f85eb6da276b64241f3f21f
Simranjit Kaur <simranjit.kaur@udaan.com>
2021-05-15 12:11:52 +0530

    Changed with usage of Provides and GuiceModule

--- src/main/kotlin/CheckoutService.kt
+++ src/main/kotlin/CheckoutService.kt
@@ -1,11 +1,11 @@
 import com.google.inject.Inject
 import com.google.inject.Provider
 
-class CheckoutService @Inject constructor(private val discountable: Provider<Discountable>)
+class CheckoutService @Inject constructor(private val discountable: Discountable)
 {
     fun checkout(shoppingCartTotal : Double) : Double
     {
-        val totalAfterDiscount = shoppingCartTotal - (shoppingCartTotal * discountable.get().getDiscount())
+        val totalAfterDiscount = shoppingCartTotal - (shoppingCartTotal * discountable.getDiscount())
         println("totalAfterDiscount: $totalAfterDiscount")
 
         return totalAfterDiscount
